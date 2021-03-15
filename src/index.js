c28b4201a7bbd99d5669ecfbc4f74982e424bfff
dmp11 <pd8@mail.ru>
2021-03-15 23:35:13 +0300

    feat: insert 3 functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+	if(array !== undefined){
+		if(array.length > 0){
+			let num = array[0];
+			for(let i=0; i<array.length; i++){
+				if (array[i] < num) num = array[i];
+			}
+			return num;
+		}
+	}
+	return 0; 	
 }
 
 exports.max = function max (array) {
-  return 0;
+	if(array !== undefined){
+		if(array.length > 0){
+			let num = array[0];
+			for(let i=0; i<array.length; i++){
+				if(array[i] > num){
+					num = array[i];
+				}
+			}return num;
+		}
+	}
+	return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if(array !== undefined){
+		if(array.length > 0){
+			let num = array[0];
+			let i;
+			for(i=1; i<array.length; i++)
+				num += array[i];
+			return num/i;
+		}
+	}
+	return 0;
 }
